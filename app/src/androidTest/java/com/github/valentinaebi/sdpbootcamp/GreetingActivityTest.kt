5f921fda08ecca1d23018dfc268be5022c886576
Valentin Aebi <valentin.aebi@epfl.ch>
2022-02-27 15:32:32 +0100

    Clean imports

--- app/src/androidTest/java/com/github/valentinaebi/sdpbootcamp/GreetingActivityTest.kt
+++ app/src/androidTest/java/com/github/valentinaebi/sdpbootcamp/GreetingActivityTest.kt
@@ -8,9 +8,7 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
